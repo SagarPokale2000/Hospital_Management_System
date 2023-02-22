6825bac967df73b82fc9b7b5ff77205ba3d4ab78
D6_Saurabh_69750 <saptesaurabh@gmail.com>
2023-02-22 21:02:28 +0530

    employee apis bug fixes

--- HMS_Backend/HMS_apis/src/main/java/com/hms/services/EmployeeService.java
+++ HMS_Backend/HMS_apis/src/main/java/com/hms/services/EmployeeService.java
@@ -1,23 +1,14 @@
 package com.hms.services;
 
-import java.util.List;
-
 import com.hms.payloads.EmployeeDto;
-import com.hms.payloads.UserDto;
 
 public interface EmployeeService {
 				// create
-				EmployeeDto createEmployee(EmployeeDto employeeDto);
+				EmployeeDto createEmployee(EmployeeDto employeeDto,Integer userId);
 
 				// update
 				EmployeeDto updateEmployee(EmployeeDto employeeDto, Integer Id);
 
-				// delete
-				void deleteEmployee(Integer Id);
-
 				// get
 				EmployeeDto getEmployee(Integer Id);
-
-				// get All
-				List<EmployeeDto> getEmployees();
 }
--- HMS_Backend/HMS_apis/src/main/java/com/hms/services/impl/EmployeeServiceImpl.java
+++ HMS_Backend/HMS_apis/src/main/java/com/hms/services/impl/EmployeeServiceImpl.java
@@ -1,54 +1,54 @@
 package com.hms.services.impl;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hms.entities.Employee;
+import com.hms.entities.User;
 import com.hms.exceptions.ResourceNotFoundException;
 import com.hms.payloads.EmployeeDto;
 import com.hms.repository.EmployeeRepo;
+import com.hms.repository.UserRepo;
 import com.hms.services.EmployeeService;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-	
+
 	@Autowired
 	private EmployeeRepo employeeRepo;
-	
+
+	@Autowired
+	private UserRepo userRepo;
+
 	@Autowired
 	private ModelMapper modelMapper;
 
 	@Override
-	public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-		Employee emp = this.modelMapper.map(employeeDto,Employee.class);
+	public EmployeeDto createEmployee(EmployeeDto employeeDto, Integer userId) {
+		User user = this.userRepo.findById(userId)
+				.orElseThrow((() -> new ResourceNotFoundException("User", "User id", userId)));
+
+		Employee emp = this.modelMapper.map(employeeDto, Employee.class);
+		emp.setUser(user);
 		Employee addedEmp = this.employeeRepo.save(emp);
 		return this.modelMapper.map(addedEmp, EmployeeDto.class);
 	}
 
 	@Override
 	public EmployeeDto updateEmployee(EmployeeDto employeeDto, Integer Id) {
-		
+
 		Employee emp = this.employeeRepo.findById(Id)
 				.orElseThrow(() -> new ResourceNotFoundException("Employee ", "employee Id", Id));
-		
+
 		emp.setRole(employeeDto.getRole());
 		emp.setQualificaton(employeeDto.getQualificaton());
 		emp.setSalary(employeeDto.getSalary());
 		emp.setHiredate(employeeDto.getHiredate());
 		emp.setStatus(employeeDto.getStatus());
-		
+
 		Employee updatedEmp = this.employeeRepo.save(emp);
 		return this.modelMapper.map(updatedEmp, EmployeeDto.class);
-	}
-
-	@Override
-	public void deleteEmployee(Integer Id) {
-		Employee emp = this.employeeRepo.findById(Id)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee ", "employee id",Id));
-		this.employeeRepo.delete(emp);
 	}
 
 	@Override
@@ -57,13 +57,4 @@ public class EmployeeServiceImpl implements EmployeeService {
 				.orElseThrow(() -> new ResourceNotFoundException("Doctor", "doctor id", Id));
 		return this.modelMapper.map(emp, EmployeeDto.class);
 	}
-
-	@Override
-	public List<EmployeeDto> getEmployees() {
-		List<Employee> employees = this.employeeRepo.findAll();
-		List<EmployeeDto> empDtos = employees.stream().map((emp) -> this.modelMapper.map(emp, EmployeeDto.class))
-				.collect(Collectors.toList());
-		return empDtos;
-	}
-
 }
