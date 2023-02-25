18025bec0e44af76dbb786cadefc43648be48848
D6_Sagar_Pokale_70466 <spokale922.sp@gmail.com>
2023-02-25 20:29:24 +0530

    Added Axios

--- HMS_FrontEnd/hms/src/Component/Home/Home.js
+++ HMS_FrontEnd/hms/src/Component/Home/Home.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 
 function Home() {
-  return (
-    <div>
+  
+  const [user, setUser] = useState(null)
+
+  const BASE_URL = 'http://localhost:9090/api';
+
+  const MYAXIOIS = axios.create(
+    {
+        baseURL: BASE_URL
+    }
+);
+
+ const getUser = () => {
+  return MYAXIOIS.get(`/users/1`).then((resp) => resp.data)
+}
+
+useEffect(() => {
+  getUser().then(data => {
+    setUser({ ...data })
+    console.log(data);
+    // console.log(user);
+    // debugger
+  })
+}, [])
+
+
+return (
+  <div>
     Hello
+
+    {user?.id}
+
+
+    {user?.firstName}
+    <br>
+    </br>
+    {user?.lastName}
+    <br>
+    </br>
+    {user?.id}
     </div>
   )
+
+
 }
 
 export default Home
