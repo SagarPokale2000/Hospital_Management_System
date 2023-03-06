ffbd5d86cab6622fc5a8977a7a4004d308283309
D6_Saurabh_69750 <saptesaurabh@gmail.com>
2023-03-06 16:22:45 +0530

    assign doctor page added

--- HMS_FrontEnd/hms/src/Component/Pages/Receptionist/AppointmentList.jsx
+++ HMS_FrontEnd/hms/src/Component/Pages/Receptionist/AppointmentList.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Container, Table } from "reactstrap";
-// import { loadAllEmployee, loadAllPatient } from "../../../ServerCall/Admin/Admin";
+import { Container, Table,Button,Modal, ModalHeader, ModalBody,Input,Label } from "reactstrap";
+import { PrivateAxios } from "../../../ServerCall/Axios/AxiosHelper";
 import { GetAllAppointmentList } from "../../../ServerCall/Receiptionist/ReceptionistAxios";
 import Base from "../../Base/Base";
 
@@ -15,10 +16,22 @@ function AppointmentList() {
     pageNumber: "",
   });
 
+  const [pat, setPat] = useState({
+    id:"",
+    appointmentDate: "",
+    appointmentTime: "",
+    symptoms: "",
+    doctorId:""
+  });
+
+  const [doctor, setDoctor] = useState([]);
+
+  const navigate = useNavigate()
   useEffect(() => {
     // load post of postId
     GetAllAppointmentList()
       .then((serverData) => {
+        //debugger;
         setData({
           // Concatinent the pageContent with new data -> new data with existing data
           content: [...data.content, ...serverData.content],
@@ -29,42 +42,97 @@ function AppointmentList() {
           pageNumber: serverData.pageNumber,
           
         });
-
-        // console.log(serverData);
-        // setData(serverData);
-        //debugger;
       })
-
       .catch((error) => {
         console.log(error);
         toast.error("Error in loading");
       });
   }, []);
+  const [modal, setModal] = useState(false);
+  const toggle = () => {
+      setModal(!modal);
+  }
+  
+  const resetData = () => {
+    pat.id = "";
+    pat.appointmentDate = "";
+    pat.appointmentTime = "";
+    pat.symptoms = "";
+    pat.doctorId = "";
+    setPat({});
+    setDoctor([]);
+  };
 
+  const getHealthHistory = (id) => {
+    resetData();
+    pat.id = id;
+    PrivateAxios.get(`patient/`+ id+`/healthhistory/accountant`).then((response) => {
+      var result = response.data;
+      //debugger;
+      pat.appointmentDate = result.appointmentDate;
+      pat.appointmentTime = result.appointmentTime;
+      pat.symptoms = result.symptoms;
+      setPat({...pat,"symptoms":result.symptoms});
+    })
+    getAllDocotors();
+  }
 
+  const getAllDocotors= () => {
+    PrivateAxios.get(`doctors`).then((response) => {
+      var result = response.data;
+      setDoctor(result);
+      //debugger;
+    })
+    toggle();
+  }
+
+  const confirmDoctor = () => {
+    ///patients/{patientId}/doctor/{doctorId}
+    debugger;
+    PrivateAxios.put(`patients/`+pat.id+`/doctor/`+pat.doctorId).then((response) => {
+      //var result = response.data;
+      //debugger;
+      toggle();
+      toast.success("Doctor updated Successfully");
+    })
+    resetData();
+  }
+
+  const AppointDoctor = (event) => {
+    debugger;
+    pat.doctorId = event.target.value;
+    //setPat({...pat,"doctorId":event})
+  }
+
+  const dash = () => {
+    navigate('/user/Receptionist')
+}
   console.log(data?.content[0]?.admitStatus);
   //debugger
   const user = data?.content;
-   debugger;
+   //debugger;
 
   return (
     <div>
       <Base>
         <br />
         <br />
-        <br />
+        <br /> <Button
+                    onClick={dash}
+                    className='btn btn-sm btn-info'>
+                    Back
+                  </Button>
         <Container>
           <Table hover responsive size="" striped className="w-100  p-3">
             <thead>
               <tr>
-                <th>#</th>
+                <th>Patient Id</th>
                 <th>Name</th>
-                <th>AppointmentDate</th>
-                <th>AppointmentTime</th>
-                <th>Symptoms</th>
-                <th>MobileNo</th>
-                <th>Appointed Doctor</th>
-                <th>Update</th>
+                  <th>Gender</th>
+                  <th>DOB</th>
+                  <th>Contact</th>
+                  <th>E-Mail</th>
+                <th>Assign Doctor</th>
               </tr>
             </thead>
 
@@ -73,32 +141,88 @@ function AppointmentList() {
               data && data?.content?.map((user) => {
                 return (
                   <tr key={user?.user.id}>
-                    <th scope="row">{user?.user.id}</th>
-                    <td>{user?.user.firstName}</td>
-                    <td>{user?.user.appointmentDate}</td>
-                    <td>{user?.user.appointmentTime}</td>
-                    <td>{user?.user.symptoms}</td>
-                    <td>{user?.user.mobileNo}</td>
-                    <td>{user?.user.doctor}</td>
-                    {/* <td>{user?.user.bloodGroup}</td> */}
+                    <th scope="row">{user?.id}</th>
+                    <td>{user?.user?.firstName + " "+user?.user?.lastName }</td>
+                    <td>{user?.user?.gender}</td>
+                    <td>{user?.user?.dob}</td>
+                    <td>{user?.user?.mobileNo}</td>
+                    <td>{user?.user?.email}</td>
+                    <td><Button
+                    onClick={() => { getHealthHistory(user?.id) }}
+                    style={styles.button}
+                    className='btn btn-sm btn-success'>
+                    Appoint Doctor
+                  </Button></td>
                   </tr>
-                  
                 );
               })
-              
-              
-              
-              
               }
-
-             
             </tbody>
-
           </Table>
         </Container>
+        <Modal isOpen={modal} toggle={toggle} centered={true} scrollable={true} size={"md"}>
+          <ModalHeader toggle={toggle}>Update Patient Payment</ModalHeader>
+          <ModalBody>
+              <>
+                <Table>
+                  <tbody>
+                <tr>
+                    <th>Appointment Date  = </th>
+                    <td>{pat.appointmentDate}</td>
+                  </tr>
+                  <tr>
+                    <th>Appointment Time  = </th>
+                    <td>{pat.appointmentTime}</td>
+                    </tr>
+                    <tr>
+                    <th>Symptoms = </th>
+                    <td>{pat.symptoms}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                    <Label for="doctor" >Select Doctor</Label>
+                            <Input
+                                type="select"
+                                id="doctor"
+                                placeholder="Enter here"
+                                className="rounded-0"
+                                name="id"
+                        onChange={(e) => { AppointDoctor(e) }}
+                                defaultValue={0}
+                            >
+                       <option disabled value={0} >--Select Docotor--</option>
+                        {
+                          doctor.map((d) => (
+                            <option value={d?.id} key={d?.id}>
+                              {d.employee.user.firstName + " " + d.employee.user.lastName}
+                            </option>
+                          ))
+                        }
+                      </Input>
+                    </td>
+                  </tr>
+                    <tr>
+                    <th><Button outline color="primary" onClick={() => { confirmDoctor() }}> 
+                  Allocate Doctor
+                      </Button></th>
+                      <th></th>
+                    </tr>
+                  </tbody>
+                </Table>
+                </>
+          </ModalBody>            
+          </Modal>
       </Base>
     </div>
   );
 }
-
+const styles = {
+  h3: {
+    textAlign: 'center',
+    margin: 20,
+  },
+  button: {
+    marginRight: 10,
+  },
+}
 export default AppointmentList;
